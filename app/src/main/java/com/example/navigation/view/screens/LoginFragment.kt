c964d1ae37cf6f4ae7d6a761d87a7204ca989e06
MoAlqw <nikitaguzenko2@gmail.com>
2025-05-01 22:24:34 +0500

    Added simple navigation

--- app/src/main/java/com/example/navigation/view/screens/LoginFragment.kt
+++ app/src/main/java/com/example/navigation/view/screens/LoginFragment.kt
@@ -1,6 +1,34 @@
 package com.example.navigation.view.screens
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.navigation.R
+import com.example.navigation.databinding.FragmentLoginBinding
 
-class LoginFragment: Fragment(R.layout.fragment_login)
+class LoginFragment: Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
+        }
+    }
+
+}
--- app/src/main/java/com/example/navigation/view/screens/SplashFragment.kt
+++ app/src/main/java/com/example/navigation/view/screens/SplashFragment.kt
@@ -1,6 +1,19 @@
 package com.example.navigation.view.screens
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.navigation.R
 
-class SplashFragment : Fragment(R.layout.fragment_splash)
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }, 2000)
+    }
+
+}
